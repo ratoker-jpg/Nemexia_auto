45357e826d84708e185e7e23a87e4fb623be6e1c
ratoker-jpg <ratoker@gmail.com>
2026-08-08 11:14:43 +0300

    V2-01: add isolated V2 package

---
+++ v2/__init__.py
@@ -0,0 +1,7 @@
+"""Nemexia Raid Manager V2 foundation.
+
+The legacy Tkinter application does not import this package. V2 code is kept
+side-by-side until functional parity is proven.
+"""
+
+V2_DATA_DIR_NAME = "NemexiaRaidManagerV2"
